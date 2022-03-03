6872cb844cf297430ea6793203a56927d037ff9b
HunteRoi <me@tinaeldevresse.eu>
2022-03-03 21:07:28 +0100

    TCR

--- src/LoggerManager.ts
+++ src/LoggerManager.ts
@@ -1,7 +1,10 @@
+import { EventEmitter } from 'stream';
 import { IModule, LoggerOptions } from '@types';
 
-export class LoggerManager {
+export class LoggerManager extends EventEmitter {
 	constructor(options: LoggerOptions, modules: IModule[] = []) {
+		super();
+
 		if (!options) throw new Error('You must provide valid options!');
 		if (!modules) throw new Error('You must provide valid modules!');
 	}
