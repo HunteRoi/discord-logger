7af123a75942dcd6968e68620f7c7bce0e02650a
HunteRoi <me@tinaeldevresse.eu>
2022-03-03 23:53:39 +0100

    TCR

--- src/LoggerManager.ts
+++ src/LoggerManager.ts
@@ -6,6 +6,9 @@ import { IEventParser, IModule, LoggerOptions } from '@types';
 
 export class LoggerManager extends EventEmitter {
 	private readonly _eventParser: IEventParser;
+	private readonly _client: Client;
+	private readonly _options: LoggerOptions;
+	private readonly _modules: IModule[];
 
 	constructor(options: LoggerOptions, client: Client, modules: IModule[] = []) {
 		super();
@@ -13,6 +16,9 @@ export class LoggerManager extends EventEmitter {
 		if (!client) throw new Error('You must provide a client!');
 		if (!modules) throw new Error('You must provide valid modules!');
 
+		this._options = options;
 		this._eventParser = new EventParser();
+		this._client = client;
+		this._modules = modules;
 	}
 }
