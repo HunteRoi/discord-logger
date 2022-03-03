51d40209a99962c4f6794f75b81bb8612db08ee7
HunteRoi <me@tinaeldevresse.eu>
2022-03-03 21:55:13 +0100

    TCR

--- src/LoggerManager.ts
+++ src/LoggerManager.ts
@@ -1,12 +1,18 @@
 import { EventEmitter } from 'stream';
 import { Client } from 'discord.js';
-import { IModule, LoggerOptions } from '@types';
+
+import { EventParser } from './EventParser';
+import { IEventParser, IModule, LoggerOptions } from '@types';
 
 export class LoggerManager extends EventEmitter {
+	private readonly _eventParser: IEventParser;
+
 	constructor(options: LoggerOptions, client: Client, modules: IModule[] = []) {
 		super();
 		if (!options) throw new Error('You must provide valid options!');
 		if (!client) throw new Error('You must provide a client!');
 		if (!modules) throw new Error('You must provide valid modules!');
+
+		this._eventParser = new EventParser();
 	}
 }
