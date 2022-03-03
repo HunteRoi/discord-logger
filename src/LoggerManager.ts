b679b15de8c85a55e1cd13c42227fee35cd6dee8
HunteRoi <me@tinaeldevresse.eu>
2022-03-03 21:40:28 +0100

    TCR

--- src/LoggerManager.ts
+++ src/LoggerManager.ts
@@ -1,11 +1,12 @@
 import { EventEmitter } from 'stream';
+import { Client } from 'discord.js';
 import { IModule, LoggerOptions } from '@types';
 
 export class LoggerManager extends EventEmitter {
-	constructor(options: LoggerOptions, modules: IModule[] = []) {
+	constructor(options: LoggerOptions, client: Client, modules: IModule[] = []) {
 		super();
-
 		if (!options) throw new Error('You must provide valid options!');
+		if (!client) throw new Error('You must provide a client!');
 		if (!modules) throw new Error('You must provide valid modules!');
 	}
 }
--- tests/LoggerManager.spec.ts
+++ tests/LoggerManager.spec.ts
@@ -13,22 +13,28 @@ describe('LoggerManager', () => {
 	});
 
 	it('should instanciate correctly', () => {
-		expect(() => new LoggerManager(options, modules)).not.toBeNull();
+		expect(() => new LoggerManager(options, client, modules)).not.toBeNull();
 	});
 
 	it('should throw error when "options" is falsy', () => {
-		expect(() => new LoggerManager(null, modules)).toThrowError(
+		expect(() => new LoggerManager(null, client, modules)).toThrowError(
 			'You must provide valid options!'
 		);
 	});
 
 	it('should throw error when "modules" is undefined or null', () => {
-		expect(() => new LoggerManager(options, null)).toThrowError(
+		expect(() => new LoggerManager(options, client, null)).toThrowError(
 			'You must provide valid modules!'
 		);
 	});
 
 	it('should not throw error when "modules" is not defined', () => {
-		expect(() => new LoggerManager(options)).not.toBeNull();
+		expect(() => new LoggerManager(options, client)).not.toBeNull();
+	});
+
+	it('should throw error when "client" is undefined or null', () => {
+		expect(() => new LoggerManager(options, null)).toThrowError(
+			'You must provide a client'
+		);
 	});
 });
