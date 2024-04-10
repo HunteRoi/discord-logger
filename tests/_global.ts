8c8a9fa3b58ca34c5a0c17d56d501768cc765af6
HunteRoi <me@tinaeldevresse.eu>
2024-04-10 18:01:12 +0200

    chore: use biomejs instead of eslint

--- tests/_global.ts
+++ tests/_global.ts
@@ -1,17 +1,17 @@
-import { ClientEvents } from 'discord.js';
-import { IModule } from '../src';
+import type { ClientEvents } from "discord.js";
+import type { IModule } from "../src";
 
 export function generateTestModule(): Partial<IModule> {
-  const event = getTestEvent();
-  return {
-    [`on_${event}`]: () => undefined,
-  };
+    const event = getTestEvent();
+    return {
+        [`on_${event}`]: () => undefined,
+    };
 }
 
 export function generateTestModules(): Partial<IModule>[] {
-  return [generateTestModule(), generateTestModule(), generateTestModule()];
+    return [generateTestModule(), generateTestModule(), generateTestModule()];
 }
 
 export function getTestEvent(): keyof ClientEvents {
-  return 'channelCreate';
+    return "channelCreate";
 }
